c769274a0bba9f4ef987a43eb9a13420941d4eb3
79283 <792836909@qq.com>
2024-08-19 12:06:44 +0800

    操作日志

---
+++ src/main/java/com/cn/jmw/OperatorFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.cn.jmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperatorFrameworkApplication.class, args);
+	}
+
+}
